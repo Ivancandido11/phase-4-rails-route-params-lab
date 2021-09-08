c0d38c96083f78e38072e5714f6dfd9e7994cba5
Ivan Candido <ivan.candido@powerhrg.com>
2021-09-08 10:24:03 -0400

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,17 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
-    render json: students
+    if params[:name]
+      found_student = students.filter { |student| student.to_s.downcase.include?(params[:name]) }
+      render json: found_student
+    else
+      render json: students
+    end
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
